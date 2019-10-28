963c727fe4d038210d0e8ae15c26de8f50692512
Tammie Chung <tammie.chung11@gmail.com>
2019-10-28 12:42:43 -0400

    Create Button and DayListItem using storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,20 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from 'classnames';
+// const classNames = require('classnames');
 
 export default function Button(props) {
-   return <></>;
+
+  const buttonClass = classNames('button', {
+    'button--confirm': props.confirm,
+    'button--danger': props.danger
+  });
+
+  return <button 
+    disabled={props.disabled} 
+    onClick={props.onClick} 
+    className={buttonClass}
+    >
+      {props.children}
+    </button>;
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import 'components/DayListItem.scss';
+import classNames from 'classnames';
+
+export default function DayListItem(props) {
+
+  const dayListItemClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': (props.spots === 0)
+  })
+
+  return (
+    <li class={dayListItemClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
