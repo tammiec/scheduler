f8e217ad6d24f03fae57b4bb69a1a51375020e3f
Tammie Chung <tammie.chung11@gmail.com>
2019-10-29 14:31:59 -0400

    Refactor to call API for appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,58 +3,29 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from './DayList';
 import Appointment from 'components/Appointment';
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Bob Smith",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  }
-];
+import { getAppointmentsForDay } from "../helpers/selectors";
 
 export default function Application(props) {
 
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState('Monday');
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  // const setDays = days => setState(prev => ({...prev, days}));
 
   useEffect(() => {
-    axios
-      .get('/api/days')
-      .then(res => {
-        setDays(res.data);
-      })
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments')
+    ]).then(all => {
+      setState(prev => ({days: all[0].data, appointments: all[1].data}));
+    })
   }, [])
+
+  const appointments = getAppointmentsForDay(state, state.day);
 
   return (
     <main className="layout">
@@ -67,8 +38,8 @@ export default function Application(props) {
           <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
             <DayList 
-              days={days}
-              day={day}
+              days={state.days}
+              day={state.day}
               setDay={setDay}
             />
           </nav>
