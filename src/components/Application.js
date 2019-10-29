6398d1faf9c7c7a9fe28a5401d59ca308f93f81e
Tammie Chung <tammie.chung11@gmail.com>
2019-10-29 15:57:03 -0400

    Retrieve interviewer data and create getInterview selector function

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,14 +3,15 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from './DayList';
 import Appointment from 'components/Appointment';
-import { getAppointmentsForDay } from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
 export default function Application(props) {
 
   const [state, setState] = useState({
     day: 'Monday',
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -19,13 +20,27 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
-      axios.get('/api/appointments')
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then(all => {
-      setState(prev => ({days: all[0].data, appointments: all[1].data}));
-    })
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    }).catch(err => console.log(err));
   }, [])
 
   const appointments = getAppointmentsForDay(state, state.day);
+  const schedule = appointments.map(appointment => {
+
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment 
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    )
+  })
 
   return (
     <main className="layout">
@@ -51,14 +66,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         <ul>
-          {appointments.map(appointment => {
-            return (
-              <Appointment 
-                key={appointment.id}
-                {...appointment}
-              />
-            )
-          })}
+          {schedule}
           <Appointment key="last" time="5pm" />
         </ul>
       </section>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -14,3 +14,17 @@ export function getAppointmentsForDay(state, name) {
   return apptsDetails;
 }
 
+export function getInterview(state, interview) {
+  const apptInfo = Object.values(state.appointments);
+  if (!interview || typeof interview.interviewer !== 'number') {
+    return interview;
+  } else {
+    let filteredInterview = apptInfo.filter(appt => appt.interview === interview);
+    filteredInterview = filteredInterview[0].interview;
+    if (!filteredInterview) {
+      return null;
+    }
+    filteredInterview.interviewer = state.interviewers[filteredInterview.interviewer];
+    return filteredInterview;
+  }
+}
