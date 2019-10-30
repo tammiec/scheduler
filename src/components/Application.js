c174b0843c77e9bcda483c579fbbffcbb03dbcdf
Tammie Chung <tammie.chung11@gmail.com>
2019-10-30 16:38:44 -0400

    Fix put request bug

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,72 +1,21 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+// import React, { useState, useEffect } from "react";
+import React from 'react';
+// import axios from 'axios';
 import "components/Application.scss";
 import DayList from './DayList';
 import Appointment from 'components/Appointment';
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors";
+import useApplicationData from 'hooks/useApplicationData';
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({...prev, days}));
-
-  // Call API to get data and set state
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then(all => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    }).catch(err => console.log(err));
-  }, [])
+  const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
 
   // get array of appointments for any given day
   const appointments = getAppointmentsForDay(state, state.day);
 
   // get array of interviewers for any given day
   const interviewers = getInterviewersForDay(state, state.day);
-
-  // Book Interview Function
-  const bookInterview = (id, interview) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: {...interview}
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-    return axios
-      .put(`/api/appointments/${id}`, appointment)
-      .then((res) => {
-        setState({...state, appointments});
-      });
-  }
-
-  // Cancel Interview Function
-  const cancelInterview = (id) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-    return axios
-      .delete(`/api/appointments/${id}`)
-      .then(res => {
-        setState({...state, appointments})
-      });
-  }
 
   // create Appointment component per appointment that day
   const schedule = appointments.map(appointment => {
--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -34,7 +34,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={() => cancel()} danger>Cancel</Button>
-          <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
+          <Button onClick={() => props.onSave(name, interviewer.id)} confirm>Save</Button>
         </section>
       </section>
     </main>
